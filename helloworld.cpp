cff2a45a1ae3395f9c12d419f617e63fa4170de3
Mahathir <mahathirs2004@gmail.com>
2026-06-25 18:33:37 -0400

    Added Python support

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+int main() {
+    // this is a comment
+    std :: cout << "Hello World!" << std :: endl;
+    std :: cout << " This is very cool" << std :: endl ;
+
+    double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std::cout << circumference << "cm";
+    return 0;
+}
